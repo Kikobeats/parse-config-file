9768bcccae2f3c710e1539d6492e65ea35fc2f16
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-04 11:15:36 +0200

    First commit

---
+++ test/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var should = require('should')
+var path = require('path')
+var parse = require('..')
+var fs = require('fs')
+
+var PATH = {
+  json: fs.readFileSync(path.resolve('test/fixtures/json'), 'utf8'),
+  cson: fs.readFileSync(path.resolve('test/fixtures/cson'), 'utf8'),
+  yaml: fs.readFileSync(path.resolve('test/fixtures/yaml'), 'utf8')
+}
+
+function expected (data) {
+  Object.keys(data).length.should.be.equal(2)
+}
+
+describe('load-conf-file', function () {
+  Object.keys(PATH).forEach(function (filetype) {
+    it(filetype, function () {
+      var data = parse(PATH[filetype])
+      expected(data)
+    })
+  })
+})
